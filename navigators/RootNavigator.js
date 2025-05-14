e94841e973c3258298615aa19d64217cbf87045c
Muhammad Abid <abidroid1@gmail.com>
2025-05-14 12:25:41 +0500

    navigation

--- navigators/RootNavigator.js
+++ navigators/RootNavigator.js
@@ -1,16 +1,23 @@
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import T20LandingScreen from "../screens/t20/T20LandingScreen";
+import DashboardScreen from "../screens/DashboardScreen";
 
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
     return (<Stack.Navigator>
-        <Stack.Screen 
-        name="T20Landing"
-        component={T20LandingScreen}
-        
+        <Stack.Screen
+            name="Dashboard"
+            component={DashboardScreen}
+        />
+
+
+        <Stack.Screen
+            name="T20Landing"
+            component={T20LandingScreen}
+
         />
     </Stack.Navigator>);
 }
